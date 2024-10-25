1f369d29c7ddfc6e06d9dd75e7be624198e00bd9
Rafael Issao Miyagawa <rafael.issao@gmail.com>
2024-10-25 16:55:11 -0300

    Initial commit with common item example data table

---
+++ src/test/groovy/com/gildedrose/CommonItemSpecification.groovy
@@ -0,0 +1,35 @@
+package com.gildedrose
+
+import spock.lang.Specification
+
+class CommonItemSpecification extends Specification {
+    def "No final do dia, sellIn e quality deve decrementar em 1"() {
+        where:
+        sellIn | expectedSellIn | quality | expectedQuality
+        10     | 9              | 10      | 9
+        9      | 8              | 8       | 7
+        1      | 0              | 1       | 0
+    }
+
+    def "Se sellIn for menor que zero, quality deve diminuir duas vezes mais rapido"() {
+        where:
+        sellIn | expectedSellIn | quality | expectedQuality
+        0      | -1             | 10      | 8
+        -1     | -2             | 8       | 6
+        -10    | -11            | 2       | 0
+    }
+
+    def "Quality não pode ser negativa"() {
+        where:
+        sellIn | expectedSellIn | quality | expectedQuality
+        // Casos onde quality decrementa 1
+        5      | 4              | 0       | 0
+        1      | 0              | 0       | 0
+
+        // Casos onde quality diminui duas vezes mais rápido
+        0      | -1             | 1       | 0
+        -5     | -6             | 1       | 0
+        0      | -1             | 0       | 0
+        -5     | -6             | 0       | 0
+    }
+}
